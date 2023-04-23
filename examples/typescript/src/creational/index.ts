a270458072d1711a7ada7328eacf2b9006bbf862
aburd <aaron.burdick@protonmail.com>
2023-04-24 00:51:32 +0900

    add typescript

--- examples/typescript/src/creational/index.ts
+++ examples/typescript/src/creational/index.ts
@@ -1,1 +1,56 @@
-console.log('test creational')
+interface Button {
+  render(): string;
+}
+
+interface Dialog {
+  createButton(): Button;
+  render(): string;
+}
+
+class BaseDialog implements Dialog {
+  createButton() {
+    return {
+      render() {
+        return "****\nSome button\n****"
+      }
+    }
+  } 
+
+  render() {
+    let button = this.createButton();
+    return button.render()
+  }
+}
+
+class ADialog extends BaseDialog {
+  createButton() {
+    return {
+      render() {
+        return "****\nSome A button\n****"
+      }
+    }
+  } 
+}
+
+class BDialog extends BaseDialog {
+  createButton() {
+    return {
+      render() {
+        return "=====\nSome B button\n====="
+      }
+    }
+  } 
+}
+
+const env = process.env.ENVIRONMENT || "";
+let dialog: Dialog;
+
+if (env === "a") {
+  dialog = new ADialog(); 
+} else if (env === "b") {
+  dialog = new BDialog(); 
+} else {
+  dialog = new BaseDialog();
+}
+
+console.log(dialog.render());
